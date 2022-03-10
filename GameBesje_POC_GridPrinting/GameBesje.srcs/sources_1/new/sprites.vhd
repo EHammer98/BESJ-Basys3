1c131e27cddbba63903a2ac618c39df6840274eb
Elwin Hammer <elwinhammer@gmail.com>
2022-03-10 15:20:26 +0100

    New project

---
+++ GameBesje_POC_GridPrinting/GameBesje.srcs/sources_1/new/sprites.vhd
@@ -0,0 +1,88 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+package p_sprite is
+    type darray is array (0 to 31, 0 to 23) of integer; 
+end package p_sprite;
+
+use work.p_sprite.all;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity sprites is
+    Port( 
+        reset:      in std_logic;
+        clk25:      in std_logic;
+        enable:     in std_logic;
+        b1:         in std_logic;
+        b2:         in std_logic;
+        arrayOut:   out darray
+    );
+end sprites;
+
+architecture Behavioral of sprites is
+
+begin
+
+    process(clk25, reset)
+    
+    variable conf_array: darray := (others => (others => 0));
+    
+    begin
+        if reset = '1' then
+            
+        
+        elsif rising_edge(clk25) then  
+            conf_array(0,2) := 1;
+            conf_array(1,2) := 1;
+            conf_array(2,2) := 1;
+            conf_array(3,2) := 1;
+            conf_array(4,2) := 1;
+            
+            conf_array(16,5) := 2;
+            conf_array(17,5) := 2;
+            conf_array(18,5) := 2;
+            conf_array(19,5) := 2;
+            conf_array(20,5) := 2;
+            conf_array(21,5) := 2;
+            conf_array(22,5) := 2;
+            conf_array(23,5) := 2;
+            conf_array(24,5) := 2;
+            conf_array(25,5) := 2;
+            conf_array(26,5) := 2;
+            conf_array(27,5) := 2;
+            conf_array(28,5) := 2;
+            conf_array(29,5) := 2;
+            conf_array(30,5) := 2;
+            conf_array(31,5) := 2;
+            
+            conf_array(3,20) := 3;
+            
+            conf_array(28,20) := 4;
+            
+            conf_array(17,8) := 5;
+            conf_array(2,22) := 6;
+            
+            conf_array(28,19) := 7;
+            conf_array(28,18) := 7;
+            conf_array(28,17) := 7;
+            conf_array(28,16) := 7;
+            
+            conf_array(3,19) := 8;
+            conf_array(3,18) := 8;
+            conf_array(3,17) := 8;
+            conf_array(3,16) := 8;
+            
+            
+            conf_array(31,23) := 9;
+            conf_array(30,23) := 10;
+            
+            arrayOut <= conf_array;
+        end if;
+    end process;
+end Behavioral;
---
+++ GameBesje_POC_GridPrinting/GameBesje.srcs/sources_1/new/top.vhd
@@ -0,0 +1,95 @@
+library IEEE; use IEEE.STD_LOGIC_1164.ALL; use IEEE.NUMERIC_STD.ALL;
+library UNISIM; use UNISIM.VComponents.all;
+
+package p_Top is
+    type darray is array (0 to 31, 0 to 23) of integer;
+    type ROMtype is array (0 to 399) of STD_LOGIC_VECTOR(2 downto 0);
+end package p_Top;
+
+use work.p_Top.all;
+
+library IEEE; use IEEE.STD_LOGIC_1164.ALL; use IEEE.NUMERIC_STD.ALL;
+library UNISIM; use UNISIM.VComponents.all;
+
+entity top is
+    Port( 
+        clk :   in std_logic;
+        reset:  in std_logic;
+        button1: in std_logic;
+        button2: in std_logic;
+        colours: out std_logic_vector(2 downto 0) := (others => '0');
+        h_sync: out std_logic;
+        v_sync: out std_logic
+    );
+end Top;
+
+architecture Behavioral of top is
+
+
+component VGA is
+    Port ( 	
+        clk25 :              in STD_LOGIC;
+        reset :              in STD_LOGIC;
+        arrayIn :            in darray;
+        red, green, blue :   out STD_LOGIC;
+        hsync, vsync :       out STD_LOGIC;
+        enable:              out std_logic
+    );
+end component;
+
+component sprites is
+    Port( 
+        reset:      in std_logic;
+        clk25:      in std_logic;
+        enable:     in std_logic;
+        b1:         in std_logic;
+        b2:         in std_logic;
+        arrayOut:   out darray
+    );
+end component;
+
+component Prescaler25 is
+  Port ( 
+    clk_25 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end component;
+
+signal tmp_colour : std_logic_vector(2 downto 0);
+signal tmp_loc : integer;
+signal tmp_clk25 : std_logic;
+signal tmp_en : std_logic;
+signal tmp_code : integer;
+signal tmp_array : darray;
+
+begin
+
+Pre0: Prescaler25 port map (
+    clk_25 => tmp_clk25,
+    reset => reset,
+    clk_in1 => clk
+);
+
+SP0: sprites port map (
+    clk25 => tmp_clk25,
+    reset => reset,
+    enable => tmp_en,
+    b1 => button1,
+    b2 => button2,
+    arrayOut => tmp_array
+);
+
+VGA0: VGA port map(
+    clk25 => tmp_clk25,
+    reset => reset,
+    arrayIn => tmp_array,
+    red => colours(2),
+    green => colours(1),
+    blue => colours(0),
+    hsync => h_sync,
+    vsync => v_sync
+);
+
+end Behavioral;
